a7b76c1af17a523487d30238d1a12898e726863d
Michele Bortolomeazzi <michele.bortolomeazzi@dkfz-heidelberg.de>
2024-07-26 18:35:25 +0200

    Update setup.py to v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omero-vitessce",
-    version="0.1.0",
+    version="0.2.0",
     description="OMERO Vitessce multimodal data viewer plugin for OMERO.web",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
