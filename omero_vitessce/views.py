20684ef0723c2768ba5e70c8b40a4045caae6593
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-07-30 12:12:37 +0200

    Refactor with get_attached_configs.

--- omero_vitessce/views.py
+++ omero_vitessce/views.py
@@ -5,26 +5,12 @@ from omeroweb.decorators import login_required
 
 from . import omero_vitessce_settings
 
+from vitessce import VitessceConfig, ViewType as vt, FileType as ft, CoordinationType as ct
 
 # Get the address of omeroweb from the config
 SERVER = omero_vitessce_settings.SERVER_ADDRESS[1:-1]
 
-
-# login_required: if not logged-in, will redirect to webclient
-# login page. Then back to here, passing in the 'conn' connection
-# and other arguments **kwargs.
-@login_required()
-def vitessce_index(request, conn=None, **kwargs):
-    # Render the html template and return the http response
-    return render(request, "omero_vitessce/index.html")
-
-
-@login_required()
-def vitessce_panel(request, obj_type, obj_id, conn=None, **kwargs):
-    # Get all .json.txt attachements and generate links for them
-    # This way the config files can be served as text
-    # to the config argument of the vitessce webapp
-    obj_id = int(obj_id)
+def get_attached_configs(obj_type, obj_id, conn):
     obj = conn.getObject(obj_type, obj_id)
     config_files = [i for i in obj.listAnnotations()
                     if i.OMERO_TYPE().NAME ==
@@ -35,6 +21,23 @@ def vitessce_panel(request, obj_type, obj_id, conn=None, **kwargs):
                     if i.getFileName().endswith(".json.txt")]
     config_urls = [SERVER + "/omero_vitessce/?config=" + SERVER
                    + "/webclient/annotation/" + i for i in config_urls]
+    return config_files, config_urls
+
+
+@login_required()
+def vitessce_index(request, conn=None, **kwargs):
+    # Render the basic index page for the app
+    return render(request, "omero_vitessce/index.html")
+
+
+@login_required()
+def vitessce_panel(request, obj_type, obj_id, conn=None, **kwargs):
+    # Get all .json.txt attachements and generate links for them
+    # This way the config files can be served as text
+    # to the config argument of the vitessce webapp
+    obj_id = int(obj_id)
+
+    config_files, config_urls = get_attached_configs(obj_type, obj_id, conn)
 
     context = {"json_configs": dict(zip(config_files, config_urls)),
                "obj_type": obj_type, "obj_id": obj_id}
@@ -62,15 +65,10 @@ def vitessce_open(request, conn=None, **kwargs):
                    "obj_type": obj_type, "obj_id": obj_id}
         return render(request, "omero_vitessce/vitessce_panel.html", context)
 
-    obj = conn.getObject(obj_type, obj_id)
-    files = [i for i in obj.listAnnotations() if i.OMERO_TYPE().NAME ==
-             "ome.model.annotations.FileAnnotation_name"]
-    config_ids = [i.getId() for i in files
-                  if i.getFileName().endswith(".json.txt")]
-    if len(config_ids) > 0:
-        vitessce_url = SERVER + "/omero_vitessce/?config=" + SERVER + \
-            "/webclient/annotation/" + str(config_ids[0])
-        return HttpResponseRedirect(vitessce_url)
+    _, config_urls = get_attached_configs(obj_type, obj_id, conn)
+
+    if len(config_urls) > 0:
+        return HttpResponseRedirect(config_urls[0])
     else:
         context = {"json_configs": dict(),
                    "obj_type": obj_type, "obj_id": obj_id}
