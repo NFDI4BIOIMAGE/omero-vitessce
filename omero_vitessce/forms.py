d71c02a423ebd22b05ef2d24fc8a705c4fe690ef
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-08-15 16:10:16 +0200

    Put Config file names choice first.

--- omero_vitessce/forms.py
+++ omero_vitessce/forms.py
@@ -4,7 +4,9 @@ from django import forms
 
 
 class ConfigForm(forms.Form):
-
+    """ Form used to collect the parameters used to
+        automatically generate a config file for Vitessce
+    """
     def __init__(self, file_names, file_urls,
                  img_names, img_urls, *args, **kwargs):
         super(ConfigForm, self).__init__(*args, **kwargs)
@@ -14,6 +16,14 @@ class ConfigForm(forms.Form):
 
         self.text_choices.insert(0, ('', '---'))
 
+        # Filename for the config file
+        ts = datetime.datetime.now().strftime("%Y.%m.%d_%H%M%S")
+        filename = "VitessceConfig-" + ts + ".json.txt"
+
+        self.fields["config file name"] = forms.CharField(
+                empty_value=filename, strip=True,
+                min_length=1, max_length=40, required=False)
+
         # No empty default, we alway want an image in the config
         self.fields["image"] = forms.ChoiceField(
                 choices=self.image_choices, required=True)
@@ -21,12 +31,6 @@ class ConfigForm(forms.Form):
         # For other fields it is OK not to have an image
         self.image_choices.insert(0, ('', '---'))
 
-        ts = datetime.datetime.now().strftime("%Y.%m.%d_%H%M%S")
-        filename = "VitessceConfig-" + ts + ".json.txt"
-
-        self.fields["config file name"] = forms.CharField(
-                empty_value=filename, strip=True,
-                min_length=1, max_length=40, required=False)
         self.fields["segmentation"] = forms.ChoiceField(
                 choices=self.image_choices, required=False)
         self.fields["cell identities"] = forms.ChoiceField(
@@ -40,6 +44,7 @@ class ConfigForm(forms.Form):
         self.fields["expression"] = forms.ChoiceField(
                 choices=self.text_choices, required=False)
 
+        # Set default values for CharField fields
         self.fields["cell id column"].initial = "cell_id"
         self.fields["label column"].initial = "label"
         self.fields["config file name"].initial = filename
