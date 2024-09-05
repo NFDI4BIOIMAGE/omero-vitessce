b767dfd23ee9246ab90b5fb12ae67841fb1cc6dc
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-09-05 15:02:36 +0200

    version 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omero-vitessce",
-    version="1.0.2",
+    version="1.0.3",
     description="OMERO Vitessce multimodal data viewer plugin for OMERO.web",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
