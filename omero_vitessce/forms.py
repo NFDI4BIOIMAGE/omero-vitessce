81c73c86eaaa1ab051fe10be519d28f2102eeab2
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-09-05 14:56:36 +0200

    Adds tooltips.

--- omero_vitessce/forms.py
+++ omero_vitessce/forms.py
@@ -8,6 +8,7 @@ class ConfigForm(forms.Form):
         automatically generate a config file for Vitessce
     """
 
+    # Set default values for CharField fields
     default_cell_id_col = "cell_id"
     default_cell_label_col = "label"
     default_embedding_x_col = "UMAP_1"
@@ -16,6 +17,41 @@ class ConfigForm(forms.Form):
     default_molecule_label = "gene"
     default_molecule_x_col = "x"
     default_molecule_y_col = "y"
+
+    # Set help_text values for all fields
+
+    config_file_name_help = "Name of the config file to use, \
+            a '.json' extension is added if missing."
+    image_help = "OMERO Image to view."
+    segmentation_help = "Label image to overlay on the image, \
+            pixel values correspond to cell identities."
+    cell_identities_help = ".csv file with at least 2 columns: \
+            Cell id column and Label column defined in the 2 fields below."
+    cell_id_help = "Name of the Cell id column used in \
+            'Cell identities', 'Expression', 'Embeddings'."
+    cell_label_help = "Name of the Label used in Cell identities, \
+            uesed e.g. to distinguish different cell types."
+    expression_help = ".csv file with the Cell id column all other columns \
+            are considered as expression values and should be numerical."
+    embeddings_help = ".csv file with the Cell id column, \
+            the Embedding x and Embedding y \
+            columns defined in the 2 fields below."
+    embeddings_x_help = "Name of the Embedding x used in Embeddings."
+    embeddings_y_help = "Name of the Embedding y used in Embeddings."
+    molecules_help = ".csv file with at least 4 columns: Molecule id, \
+            label, x, y (headers in the fields below)."
+    molecule_id_help = "Name of the Molecule id column used in Molecules."
+    molecule_label_help = "Name of the Molecule label \
+            column used in Molecules."
+    molecule_x_help = "Name of the Molecule x column used in Molecules."
+    molecule_y_help = "Name of the Molecule y column used in Molecules."
+    histograms_help = "Add 3 plots showing: The number of transcripts per \
+            cell, the number of cells in each set, \
+            gene expression in each set."
+    heatmap_help = "Adds an heatmap."
+    status_help = "Adds a status panel to display info on the selected cell."
+    description_help = "Adds a description panel to display info on the \
+            dataset/image (taken from the description field)."
 
     def __init__(self, file_names, file_urls,
                  img_names, img_urls, *args, **kwargs):
@@ -28,77 +64,100 @@ class ConfigForm(forms.Form):
 
         self.fields["config_file_name"] = forms.CharField(
                 empty_value=filename, strip=True,
-                min_length=1, max_length=40, required=False)
+                min_length=1, max_length=40, required=False,
+                help_text=ConfigForm.config_file_name_help)
 
         # No empty default, we alway want an image in the config
         self.fields["image"] = forms.ChoiceField(
-                choices=self.image_choices[1:], required=True)
+                choices=self.image_choices[1:], required=True,
+                help_text=ConfigForm.image_help)
 
+        # Cell data
         self.fields["segmentation"] = forms.ChoiceField(
-                choices=self.image_choices, required=False)
+                choices=self.image_choices, required=False,
+                help_text=ConfigForm.segmentation_help)
 
         self.fields["cell_identities"] = forms.ChoiceField(
-                choices=self.text_choices, required=False)
+                choices=self.text_choices, required=False,
+                help_text=ConfigForm.cell_identities_help)
 
         self.fields["cell_id_column"] = forms.CharField(
                 empty_value=ConfigForm.default_cell_id_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.cell_id_help)
 
         self.fields["cell_label_column"] = forms.CharField(
                 empty_value=ConfigForm.default_cell_label_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.cell_label_help)
 
         self.fields["expression"] = forms.ChoiceField(
-                choices=self.text_choices, required=False)
+                choices=self.text_choices, required=False,
+                help_text=ConfigForm.expression_help)
 
         self.fields["embeddings"] = forms.ChoiceField(
-                choices=self.text_choices, required=False)
+                choices=self.text_choices, required=False,
+                help_text=ConfigForm.embeddings_help)
 
         self.fields["embedding_x"] = forms.CharField(
                 empty_value=ConfigForm.default_embedding_x_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.embeddings_x_help)
 
         self.fields["embedding_y"] = forms.CharField(
                 empty_value=ConfigForm.default_embedding_y_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.embeddings_y_help)
 
+        # Molecule data
         self.fields["molecules"] = forms.ChoiceField(
-                choices=self.text_choices, required=False)
+                choices=self.text_choices, required=False,
+                help_text=ConfigForm.molecules_help)
 
         self.fields["molecule_id"] = forms.CharField(
                 empty_value=ConfigForm.default_molecule_id, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.molecule_id_help)
 
         self.fields["molecule_label"] = forms.CharField(
                 empty_value=ConfigForm.default_molecule_label, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.molecule_label_help)
 
         self.fields["molecule_x"] = forms.CharField(
                 empty_value=ConfigForm.default_molecule_x_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.molecule_x_help)
 
         self.fields["molecule_y"] = forms.CharField(
                 empty_value=ConfigForm.default_molecule_y_col, strip=True,
-                min_length=1, max_length=20, required=False)
+                min_length=1, max_length=20, required=False,
+                help_text=ConfigForm.molecule_y_help)
 
-        self.fields["histograms"] = forms.BooleanField(initial=True,
-                                                       required=False)
-        self.fields["heatmap"] = forms.BooleanField(initial=True,
-                                                    required=False)
-        self.fields["status"] = forms.BooleanField(initial=False,
-                                                   required=False)
-        self.fields["description"] = forms.BooleanField(initial=False,
-                                                        required=False)
+        # Additional views
+        self.fields["histograms"] = \
+            forms.BooleanField(initial=True, required=False,
+                               help_text=ConfigForm.histograms_help)
+        self.fields["heatmap"] = \
+            forms.BooleanField(initial=True, required=False,
+                               help_text=ConfigForm.heatmap_help)
+        self.fields["status"] = \
+            forms.BooleanField(initial=False, required=False,
+                               help_text=ConfigForm.status_help)
+        self.fields["description"] = \
+            forms.BooleanField(initial=False, required=False,
+                               help_text=ConfigForm.description_help)
 
         # Set default values for CharField fields
         self.fields["config_file_name"].initial = filename
         self.fields["cell_id_column"].initial = ConfigForm.default_cell_id_col
-        self.fields["cell_label_column"].initial = ConfigForm.default_cell_label_col
+        self.fields["cell_label_column"].initial = \
+            ConfigForm.default_cell_label_col
         self.fields["embedding_x"].initial = ConfigForm.default_embedding_x_col
         self.fields["embedding_y"].initial = ConfigForm.default_embedding_y_col
         self.fields["molecule_id"].initial = ConfigForm.default_molecule_id
-        self.fields[
-                "molecule_label"].initial = ConfigForm.default_molecule_label
+        self.fields["molecule_label"].initial = \
+            ConfigForm.default_molecule_label
         self.fields["molecule_x"].initial = ConfigForm.default_molecule_x_col
         self.fields["molecule_y"].initial = ConfigForm.default_molecule_y_col
 
