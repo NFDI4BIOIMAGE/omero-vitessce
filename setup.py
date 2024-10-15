e0d429c92d21daeb1304b4954147d3fd48126087
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-10-15 10:45:28 +0200

    Bump version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omero-vitessce",
-    version="1.0.5",
+    version="1.0.6",
     description="OMERO Vitessce multimodal data viewer plugin for OMERO.web",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
