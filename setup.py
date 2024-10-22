56135be2bff688968ce53d37145c97b388eb719c
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2024-10-22 10:18:34 +0200

    Add shapely dependency

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/MicheleBortol/omero-vitessce",
     python_requires='>=3.9',
     packages=find_packages(),
-    install_requires=["vitessce==3.3.0"],
+    install_requires=["vitessce==3.3.0", "shapely==2.0.6"],
     keywords=["OMERO.web", "OMERO", "Vitessce", "viewer"],
     include_package_data=True,
 )
