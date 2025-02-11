6441edacd1168629f11dbf47df44ef98f4db44d1
Michele Bortolomeazzi <38500587+MicheleBortol@users.noreply.github.com>
2025-02-11 10:39:57 +0100

    Fix dependency issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omero-vitessce",
-    version="1.2.2",
+    version="1.2.3",
     description="OMERO Vitessce multimodal data viewer plugin for OMERO.web",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/MicheleBortol/omero-vitessce",
     python_requires='>=3.9',
     packages=find_packages(),
-    install_requires=["vitessce==3.3.0", "shapely==2.0.6"],
+    install_requires=["vitessce==3.5.1", "shapely==2.0.6"],
     keywords=["OMERO.web", "OMERO", "Vitessce", "viewer"],
     include_package_data=True,
 )
