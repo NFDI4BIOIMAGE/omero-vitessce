90706c3b1e44dca1ff6ec597f83841bb28a3bcfd
Michele Bortolomeazzi <michele.bortolomeazzi@dkfz.de>
2025-08-06 16:42:14 +0200

    Bump to v 1.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="omero-vitessce",
-    version="1.2.4",
+    version="1.2.5",
     description="OMERO Vitessce multimodal data viewer plugin for OMERO.web",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
